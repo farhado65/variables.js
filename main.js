16bc6b981ee3eb89f384f02adcb2bf1df6a6a679
JavaScript <javascript@Admins-iMac-8.local>
2019-12-10 20:56:45 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var name ="Radu";
+        var message = "Hello "+ name +"!";
+        var timeout = 3000;
+        var isNight = false;
+        var isMorning = false;
+        var nothing = null;
+
+        var date = new Date();
+        var hour = date.getHours();
+        
+        if(hour==0){
+            alert("Happy new year!");
+        }else if(hour >= 20 || hour <= 6) {
+        
+            alert("Nopate buna!");
+            alert("Somn usor");
+        } else if(hour <= 11){
+            alert("Buna dimineata!");
+        } else  if(hour <=17){
+            alert("Buna ziua!")
+        } else {
+            alert("Buna seara!");    
+        }
+        
+
+        setTimeout(sayHello,3000);
+        function sayHello(){
+            // alert(message);
+            console.log(message);
+        }
